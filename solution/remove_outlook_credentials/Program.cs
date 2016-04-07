2f17a395ca96fecfca625ee0909b3025914ba53f
Jeremy Mill <jeremymill@gmail.com>
2016-04-07 15:54:11 -0400

    Initial Commit

---
+++ solution/remove_outlook_credentials/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CredentialManagement;
+
+namespace remove_outlook_credentials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CredentialSet set = new CredentialSet();
+            var creds = set.Load();
+            foreach(Credential cred in creds)
+            {
+                Console.WriteLine(cred.Target);
+                if (cred.Target.Contains("MS.Outlook"))
+                {
+                    cred.Delete();
+                }
+            }
+        }
+    }
+}
